48014c994abaeacbe05380268aed52a16d82ae9d
calebgregory <calebgregory@gmail.com>
2015-07-14 11:13:39 -0500

    Created search controller and view

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,10 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', [
+               'ionic',
+               'starter.controllers',
+               'won.search'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -34,7 +37,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     url: "/search",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: "templates/search/search.html",
+        controller: 'SearchCtrl'
       }
     }
   })
@@ -52,7 +56,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       views: {
         'menuContent': {
           templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
         }
       }
     })
@@ -62,7 +65,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     views: {
       'menuContent': {
         templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
       }
     }
   });
